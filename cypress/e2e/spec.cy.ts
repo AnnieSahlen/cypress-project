b671fcbd87f97f5cc44ba1e72dccc242b502e7ba
Annie Sahlén <annieimsahlen@gmail.com>
2024-11-28 14:02:54 +0100

    minor fix

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -6,6 +6,9 @@ describe("Christmas Markets project", function () {
     cy.url().should("include", "/calendar");
     cy.contains("Home").click();
     cy.get("h1").should("include.text", "Christmas Markets");
+  });
+  it("visits the site and renders data from database", function () {
+    cy.visit("http://localhost:5173/");
     cy.get("ul").should("be.visible");
     cy.get("li").should("be.visible");
     cy.get("li").should("include.text", "Kronhusjul");
